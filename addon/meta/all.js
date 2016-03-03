8281951b033ebca8dc4e2d3332c29c6d16c25387
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-03-02 22:40:10 -0800

    Refactor meta `all`

--- addon/meta/all.js
+++ addon/meta/all.js
@@ -2,26 +2,21 @@ import { validationResult, SUBJECT_KEY } from 'computed-validator/validator';
 
 export default function all(...validationRules) {
   return function(key) {
-    let validationBlueprints = validationRules.map(rule => rule(key));
+    let validationBlueprints = validationRules.map((rule) => rule(key));
 
-    let dependentKeys = [];
-    validationBlueprints.forEach(blueprint => {
-      dependentKeys.push(...blueprint.dependentKeys);
-    });
+    let dependentKeys = validationBlueprints.reduce((accum, blueprint) => {
+      return accum.concat(blueprint.dependentKeys);
+    }, []);
 
-    let validationBlueprintFunction = function() {
-      let allErrors = [];
-      validationBlueprints.forEach((blueprint) => {
+    let fn = function() {
+      let allErrors = validationBlueprints.reduce((accum, blueprint) => {
         let validationResult = blueprint.fn.apply(this, this.get(SUBJECT_KEY), ...blueprint.dependentKeys);
-        allErrors.push(...validationResult.errors);
-      });
+        return accum.concat(validationResult.errors);
+      }, []);
 
       return validationResult(allErrors);
     };
 
-    return {
-      dependentKeys,
-      fn: validationBlueprintFunction
-    };
+    return { dependentKeys, fn };
   };
 }
