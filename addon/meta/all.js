813ad2fd6372c60eef0ee4d2a8f9ed0e1d3a6638
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-03-20 18:21:57 -0700

    Document meta/all

--- addon/meta/all.js
+++ addon/meta/all.js
@@ -2,6 +2,15 @@ import { flatMap } from 'computed-validator/utils';
 import { metaBlueprintFor } from './utils';
 import validationRule from 'computed-validator/validation-rule';
 
+/**
+ * A validation rule that synthesizes a combined validation rule from others.
+ *
+ * This rule examines the dependent keys and validate functions for other rules
+ * to build a rule that returns errors derived from running all of the functions.
+ *
+ * @param {array} validationRules - A list of validation rules declared by the user
+ * @return {object} validationBlueprint
+ */
 export default validationRule(function(validationRules, key) {
   let { dependentKeys, validateFunctions } = metaBlueprintFor(validationRules, key);
 
