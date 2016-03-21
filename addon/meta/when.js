62638561b150677f477fa412a46536739c7e9dd0
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-03-20 18:58:46 -0700

    Document when validation rule

--- addon/meta/when.js
+++ addon/meta/when.js
@@ -2,13 +2,17 @@ import Ember from 'ember';
 import validationRule from 'computed-validator/validation-rule';
 const { get } = Ember;
 
-export default validationRule(function(args, key) {
-  let [whenKey, rule] = args;
+/**
+ * This rule executes another rule only when a given key is a truthy value on
+ * the subject. This rule duplicates the functionality of the "when" option
+ * and may be removed.
+ */
+export default validationRule(function([whenKey, rule], key) {
   let { dependentKeys, validate } = rule(key);
 
   let wrappedValidate = function(subject) {
     if (get(subject, whenKey)) {
-      return validate.call(this, subject);
+      return validate(subject);
     } else {
       return [];
     }
