c00ab850d3eb5d3554bcc8e02da378698a5d64c7
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-03-23 20:27:58 -0700

    Document match

---
+++ tests/dummy/app/components/match-example.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+import {
+  computedValidator,
+  match
+} from 'computed-validator';
+
+export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    this.user = {};
+  },
+
+  layout: hbs`
+    {{validated-field
+      label="Password"
+      value=user.password
+      errors=validator.password.errors}}
+  `,
+
+  // BEGIN-SNIPPET match-example-validator
+  validator: computedValidator('user', {
+    password: match(/^(?=.*[a-z])(?=.*[A-Z]).+$/, {
+      message: "must have at least one uppercase and one lowercase letter"
+    })
+  })
+  // END-SNIPPET
+});
+
