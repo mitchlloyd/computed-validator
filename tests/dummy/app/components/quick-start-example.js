d9fda03f281354fb08ff07e0e77efdb2565a397f
Mitch Lloyd <mitch.lloyd@gmail.com>
2016-03-27 12:46:35 -0700

    Doc updates for mobile

---
+++ tests/dummy/app/components/quick-start-example.js
@@ -0,0 +1,39 @@
+// BEGIN-SNIPPET quick-start-example
+import Ember from 'ember';
+import hbs from 'htmlbars-inline-precompile';
+import {
+  computedValidator,
+  lengthBetween,
+  integer
+} from 'computed-validator';
+
+export default Ember.Component.extend({
+  init() {
+    this._super(...arguments);
+    this.user = {};
+  },
+
+  layout: hbs`
+    {{validated-field
+      label="Name"
+      value=user.name
+      errors=validator.name.errors}}
+
+    {{validated-field
+      label="Age"
+      value=user.age
+      errors=validator.age.errors}}
+
+    <button
+      class="btn btn-primary"
+      disabled={{not validator.isValid}}>
+      Submit
+    </button>
+  `,
+
+  validator: computedValidator('user', {
+    name: lengthBetween(2, Infinity),
+    age: integer()
+  })
+});
+// END-SNIPPET
